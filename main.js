29dee4e8869d9b5f140bca10b311ca052e3c3aa6
Maris Usis <marisusisme@gmail.com>
2016-10-18 10:07:33 -0400

    added initial code
    basic code

---
+++ main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+var mainWindow = null;
+
+
+//Handle all windows being closed
+app.on('window-all-closed', function() {
+	//Handle application closing on a darwin platform (macOS)
+	if (process.platform != "darwin") {
+		app.quit();
+	}
+})
+
+//Once application is ready...
+app.on('ready', function() {
+	//Create main browser window
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600
+	});
+	//Load the main HTML file
+	mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+
+	//When the main window is closed..
+	mainWindow.on('closed', function() {
+		//It no longer exists
+		mainWindow = null;
+	})
+})
